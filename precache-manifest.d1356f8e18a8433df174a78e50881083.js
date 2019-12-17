2e64a37a0a73aa8b3d2028954bc4f46617e00384
megroun <rahulabhisheknasa@gmail.com>
2019-12-17 17:47:29 +0530

    Updates

---
+++ precache-manifest.d1356f8e18a8433df174a78e50881083.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4205116a58544ffaa7f3114586f58130",
+    "url": "/megroun/yestheorybangalore/index.html"
+  },
+  {
+    "revision": "ba61829833b129344908",
+    "url": "/megroun/yestheorybangalore/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "8638aba45809cb5e7c40",
+    "url": "/megroun/yestheorybangalore/static/css/main.78ee0b5d.chunk.css"
+  },
+  {
+    "revision": "ba61829833b129344908",
+    "url": "/megroun/yestheorybangalore/static/js/2.73a5295f.chunk.js"
+  },
+  {
+    "revision": "a6c74ac0a23ee76501659976339beb2a",
+    "url": "/megroun/yestheorybangalore/static/js/2.73a5295f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8638aba45809cb5e7c40",
+    "url": "/megroun/yestheorybangalore/static/js/main.3f6991c4.chunk.js"
+  },
+  {
+    "revision": "dd8e3a10816faa8273ba",
+    "url": "/megroun/yestheorybangalore/static/js/runtime-main.fec204ab.js"
+  },
+  {
+    "revision": "9e3cf6824fc8213c584d76a6fde8978c",
+    "url": "/megroun/yestheorybangalore/static/media/16.9e3cf682.png"
+  },
+  {
+    "revision": "fac585217a0abaaffc63dd2d2c67a06c",
+    "url": "/megroun/yestheorybangalore/static/media/3.fac58521.png"
+  },
+  {
+    "revision": "f60a8f1a6524c43b5ad2fa0e65fa322a",
+    "url": "/megroun/yestheorybangalore/static/media/Instagram.f60a8f1a.jpg"
+  },
+  {
+    "revision": "c92e797c218e05d52f94bde4809d0973",
+    "url": "/megroun/yestheorybangalore/static/media/Instagramfam.c92e797c.jpg"
+  },
+  {
+    "revision": "eec686c477ad13c046195f228b339cf6",
+    "url": "/megroun/yestheorybangalore/static/media/_“Live_life_to_experience_the_unknown_and_embrace_the_random_spontaneity_of_it._Another_very_spontaneous_meet.eec686c4.jpg"
+  },
+  {
+    "revision": "2221426cabdd4a4de9c95699db9e8c0c",
+    "url": "/megroun/yestheorybangalore/static/media/about.2221426c.jpg"
+  },
+  {
+    "revision": "ebb6b0235372f57e81e9e8c58cfc7f65",
+    "url": "/megroun/yestheorybangalore/static/media/approach.ebb6b023.png"
+  },
+  {
+    "revision": "4161737f83df58c79ea6f151a4cf2a05",
+    "url": "/megroun/yestheorybangalore/static/media/coding.4161737f.png"
+  },
+  {
+    "revision": "5f0eb827ff3e98a762eb607dc6d772fa",
+    "url": "/megroun/yestheorybangalore/static/media/djParty.5f0eb827.jpg"
+  },
+  {
+    "revision": "e4d604c049d193d07e0e9413e41a357e",
+    "url": "/megroun/yestheorybangalore/static/media/meet1.e4d604c0.jpeg"
+  },
+  {
+    "revision": "4f5c0e04fb2131549005afd45e9ab42a",
+    "url": "/megroun/yestheorybangalore/static/media/mile.4f5c0e04.png"
+  },
+  {
+    "revision": "14ed728d6077776e9d85c34ccfecb31a",
+    "url": "/megroun/yestheorybangalore/static/media/quality.14ed728d.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/megroun/yestheorybangalore/static/media/reactjs.47ce6e77.png"
+  },
+  {
+    "revision": "b5e6c942391e87f96f566ff2cfea4047",
+    "url": "/megroun/yestheorybangalore/static/media/seek.b5e6c942.png"
+  },
+  {
+    "revision": "34be7c6ec36294b54581f70f2985a3cb",
+    "url": "/megroun/yestheorybangalore/static/media/services.34be7c6e.jpg"
+  },
+  {
+    "revision": "82d90d902f62f50eb9722580721f9b0d",
+    "url": "/megroun/yestheorybangalore/static/media/yesTheory.82d90d90.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aad68e9f62fbc5bbf43cb8346eef1d97.js"
+  "/megroun/yestheorybangalore/precache-manifest.d1356f8e18a8433df174a78e50881083.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/megroun/yestheorybangalore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
